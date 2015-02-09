4d5175b5b0f4606cfe9e6dc942803d11e9f0d078
Jonathan Meier <jnt.meier@gmail.com>
2015-02-09 10:54:53 +0100

    first tests with collections

---
+++ 00-collections/CollectionTest.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class CollectionTest {
+	public static void main(String[] args) {
+
+
+		System.out.println("\nhashCode tests");
+		System.out.println("--------------");
+		List a1 = new ArrayList();
+		System.out.println(a1.hashCode());
+
+		a1.add("asdf");
+		System.out.println(a1.hashCode());
+
+		a1.add("joni");
+		System.out.println(a1.hashCode());
+		
+		a1.add("test");
+		System.out.println(a1.hashCode());
+
+		List b1 = new ArrayList();
+		b1 = a1;
+		System.out.println(b1.hashCode());
+
+		System.out.println("\nlastIndexOf if not in the list:");
+		System.out.println("-------------------------------");
+		System.out.println(b1.lastIndexOf(1000));
+	}
+}
